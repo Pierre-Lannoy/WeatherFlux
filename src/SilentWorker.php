eaaa4190a52cab0f989ba47d15a16455bdb61089
Pierre Lannoy <pierre@lannoy.fr>
2021-01-23 11:38:40 +0100

    Makes Workerman\Worker silent.

---
+++ src/SilentWorker.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of WeatherFlux.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    Pierre Lannoy <https://pierre.lannoy.fr/>
+ * @copyright Pierre Lannoy <https://pierre.lannoy.fr/>
+ * @link      https://github.com/Pierre-Lannoy/WeatherFlux"
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace WeatherFlux;
+
+/**
+ * Makes Workerman\Worker silent.
+ */
+class SilentWorker extends \Workerman\Worker {
+
+	/**
+	 * Does not log.
+	 *
+	 * @param string $msg
+	 */
+	public static function log( $msg ) {
+
+	}
+
+}
